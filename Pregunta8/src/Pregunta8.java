3ea24bf0a7347b609df1c27fd3f05876f9cf4236
Juan Barrios <juanpabarriosr@gmail.com>
2017-10-21 13:48:09 -0500

    FailFastLimpio

--- Pregunta8/src/Pregunta8.java
+++ Pregunta8/src/Pregunta8.java
@@ -1,14 +1,33 @@
 public class Pregunta8 {
-
-
-	public Pregunta8() {
-		// TODO Auto-generated constructor stub
+	
+	public void FailFastSucio(int numero1, int numero2, int numero3)
+	{
+		if(numero1 > 0)
+		{
+			if(numero2 > 0) 
+			{
+				if(numero3 > 0)
+				{
+					System.out.println("Todos los números ingresado son positivos");
+				}
+				else
+				{
+					System.out.println("Nùmero negativo");
+				}
+			}
+			
+			
+		}
+	}	
+		
+	public void FailFastLimpio(int numero1, int numero2, int numero3)
+	{
+		if(numero1 < 0) System.out.println("Nùmero negativo");
+		if(numero2 < 0) System.out.println("Nùmero negativo");
+		if(numero3 < 0) System.out.println("Nùmero negativo");
+			
+		System.out.println("Todos los números ingresado son positivos");
 	}
-
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
+	
+	
---
+++ Pregunta8/src/Pregunta8Limpio.java
@@ -0,0 +1,11 @@
+public class Pregunta8Limpio {
+
+	public void RegisterUser(int numero1, int numero2, int numero3)
+	{
+		if(numero1 < 0) System.out.println("Nùmero negativo");
+		if(numero2 < 0) System.out.println("Nùmero negativo");
+		if(numero3 < 0) System.out.println("Nùmero negativo");
+		
+		System.out.println("Todos los números ingresado son positivos");
+	}
+}
