31c230787024f0e5fb29dec553d097ea29012830
Juan Barrios <juanpabarriosr@gmail.com>
2017-10-21 13:23:12 -0500

    commit

---
+++ Pregunta8/src/Pregunta8.java
@@ -0,0 +1,14 @@
+public class Pregunta8 {
+
+
+	public Pregunta8() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
